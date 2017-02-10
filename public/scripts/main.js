b934847a43d870927136f8586eca22ace24b0bd6
Michael Leon <programmermike1@gmail.com>
2017-02-10 11:40:22 -0600

    Dependencies added

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,15 @@
 var socket = io();
+var graduationDate = new Date(2017, 04, 28);
+$(document).ready(function(){
+  var regDays = $("#regularDays");
+  var today = new Date();
+  regDays.text(createMessage(today, graduationDate));
+});
+
+function createMessage(first, second){
+  return "There are " + daydiff(first, second) + " days left";
+}
+
+function daydiff(first, second) {
+    return Math.round((second-first)/(1000*60*60*24));
+}
